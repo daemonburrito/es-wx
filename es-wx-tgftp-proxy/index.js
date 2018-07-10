7fd226b2377d62047bb9b740ddefa078af0ac73a
Scott Drake <demonburrito@gmail.com>
2018-07-09 23:50:09 -0500

    Proxy server now caching
    The provided node TGFTP proxy server is now caching responses.
    
    * Set up `node-cache`, now saving the binary data in an in-memory cache,
      keyed by request path.
    * TODO `process.env` configuration.

--- es-wx-tgftp-proxy/index.js
+++ es-wx-tgftp-proxy/index.js
@@ -1,9 +1,13 @@
 /**
  * HTTP Reverse proxy for caching data from NOAA TGFTP servers
  */
-const express = require('express'),
+const stream = require('stream'),
+  express = require('express'),
   proxy = require('http-proxy-middleware'),
-  LOCALHOST = `localhost:3001`,
+  NodeCache = require( "node-cache" ),
+  cache = new NodeCache( { stdTTL: 300, checkperiod: 120 } ),
+  HOST = `localhost`,
+  PORT = `3001`,
   TGFTP_DOMAIN = `tgftp.nws.noaa.gov`,
   RADAR_PREFIX = `SL.us008001/DF.of/DC.radar`;
 
@@ -13,13 +17,50 @@ const cors = function (req, res, next) {
   next();
 };
 
-const etag = function (req, res, next) {
-  
+const cacheMiddleware = (req, res, next) => {
+  const url = req.url;
+
+  cache.get(url, (err, val) => {
+    if (!err) {
+      if (val !== undefined) {
+        console.log('responding from cache');
+        res.send(val);
+      }
+      else {
+        console.log('cache miss');
+        next();
+      }
+    }
+  });
 };
+
 
 const app = express();
 app.use(cors);
+app.use(cacheMiddleware);
+app.use(proxy(`http://${TGFTP_DOMAIN}/`, {
+  changeOrigin: true,
 
-app.use(proxy(`http://tgftp.nws.noaa.gov/`, {changeOrigin:true}));
+  // write a dump of the response stream for the cache
+  // there may be a faster way to do this, but the mix of the third party libs'
+  // api usage makes the decision complicated; it seems like `Readable().wrap`
+  // and `on('readabe')` is at least not slow?
+  selfHandleResponse: true,
+  onProxyRes: (proxyRes, req, res) => {
+    const proxyResStream = new stream.Readable().wrap(proxyRes);
+    let response = '';
+    proxyResStream.on('readable', function() {
+      while (chunk = this.read()) {
+        response += chunk;
+      }
+    });
 
-app.listen(3001);
+    proxyRes.on('end', function (...args) {
+      const responseBuffer = Buffer.from(response, 'binary');
+      cache.set(req.url, responseBuffer);
+      res.end(responseBuffer);
+    });
+  }
+}));
+
+app.listen(PORT, HOST);
